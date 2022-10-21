3286fbe320cd07099eb58f18fe09ab00f7c52049
George Stykalin <gstykalin@mail.ru>
2022-10-21 13:42:40 +0300

    add bot login

--- bot/loader.py
+++ bot/loader.py
@@ -1,8 +1,11 @@
 import os
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
+from utils.login import login
 
 
 load_dotenv()
 bot = Bot(os.getenv("BOT_TOKEN"), parse_mode="HTML")
 dp = Dispatcher(bot)
+
+token = login()
---
+++ bot/utils/login.py
@@ -0,0 +1,22 @@
+import os
+import requests
+
+
+def login() -> str:
+    data = {
+        "username": os.getenv("USERNAME"),
+        "password": os.getenv("PASSWORD")
+    }
+    response = requests.post(url=os.getenv("API_BASE_URL") + "auth/token/login/",
+                            data=data)
+
+    if response.status_code == 400:
+        response = requests.post(url=os.getenv("API_BASE_URL") + "auth/users/", 
+                                data=data)
+        response = requests.post(url=os.getenv("API_BASE_URL") + "auth/token/login/", 
+                                data=data)
+        jsonData = response.json()
+        return jsonData["auth_token"]
+    else:
+        jsonData = response.json()
+        return jsonData["auth_token"]
