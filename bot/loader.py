d8b690b11f7237f9cfc48884d225196fe0835920
George Stykalin <gstykalin@mail.ru>
2022-11-28 15:07:21 +0300

    update bot logging

--- bot/loader.py
+++ bot/loader.py
@@ -7,12 +7,15 @@ from dotenv import load_dotenv
 from utils.login import login
 import logging
 import logging.config
+from pathlib import Path
 
 
-logging.config.fileConfig("./logging.conf", 
+BASE_DIR = Path(__file__).resolve().parent
+
+logging.config.fileConfig(os.path.join(BASE_DIR, "logging.conf"), 
                           defaults={
-                              "err_log_file": "/var/log/err.log",
-                              "out_log_file": "/var/log/out.log"
+                              "err_log_file": os.path.join(BASE_DIR, "logs/err/err.log"),
+                              "out_log_file": os.path.join(BASE_DIR, "logs/out/out.log")
                           }, 
                           disable_existing_loggers=False)
 logger = logging.getLogger(f"root.{__name__}")
@@ -41,5 +44,4 @@ while django_start is False:
         token = login()
         django_start = True
     except Exception:
-        logger.error("server is not active")
         time.sleep(3)
