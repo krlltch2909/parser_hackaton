de7a7cac1238ca826cf625773a4ee856d50a58ef
KRL <angron2002@mail.ru>
2022-11-30 11:27:24 +0300

    fix api for many tags and types

--- app/main/views.py
+++ app/main/views.py
@@ -7,16 +7,18 @@ from .serializer import EventSerializer, TagSerializer, EventTypeSerializer
 from django.db.models.query import QuerySet
 
 
-def filter_by_type(types: list, events_for_filtering: QuerySet) -> QuerySet:
+def filter_by_type(types: list,
+                   events_for_filtering: QuerySet) -> QuerySet | list:
     """
     метод для фильтрации мероприятий по типу (конференция, хакатон, итд)
     :param types: список с типами которые надо получить
     :param events_for_filtering:  список мероприятий для фильтрации
-    :return: отфилтрованный по типам QuerySet или пустой список так ка совпадений не было
+    :return: отфилтрованный по типам QuerySet или пустой список так как совпадений не было
     """
     rez = []
     for type_of_event in types:
-        half_rez = events_for_filtering.filter(type_of_event=int(type_of_event))
+        half_rez = events_for_filtering.filter(
+            type_of_event=int(type_of_event))
         if len(rez) == 0:
             rez = half_rez
         else:
@@ -24,7 +26,8 @@ def filter_by_type(types: list, events_for_filtering: QuerySet) -> QuerySet:
     return rez
 
 
-def filter_by_tag(tags: list, events_for_filtering: QuerySet) -> QuerySet:
+def filter_by_tag(tags: list,
+                  events_for_filtering: QuerySet) -> QuerySet | list:
     """
      метод для фильтрации мероприятий по тегу (Искусственный интеллект, Управление данными, итд)
      :param tags: список с тегами которые надо получить
@@ -33,7 +36,9 @@ def filter_by_tag(tags: list, events_for_filtering: QuerySet) -> QuerySet:
      """
     rez = []
     for i in tags:
-        half_rez = events_for_filtering.filter(tags__tag_code=int(i))
+        half_rez = Event.objects.filter(
+            id__in=events_for_filtering.values('id'))
+        half_rez = half_rez.filter(tags__tag_code=int(i))
         if len(rez) == 0:
             rez = half_rez
         else:
@@ -42,7 +47,9 @@ def filter_by_tag(tags: list, events_for_filtering: QuerySet) -> QuerySet:
 
 
 class TypeTitleAPIView(generics.ListAPIView):
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [
+        IsAuthenticated,
+    ]
     serializer_class = EventSerializer
 
     def get_queryset(self):
@@ -58,7 +65,8 @@ class TypeTitleAPIView(generics.ListAPIView):
         tags = self.request.query_params.getlist('tags')
 
         if len(type_of_event_get) != 0:
-            rez = filter_by_type(types=type_of_event_get, events_for_filtering=all_events)
+            rez = filter_by_type(types=type_of_event_get,
+                                 events_for_filtering=all_events)
             if len(tags) != 0:
                 rez = filter_by_tag(tags=tags, events_for_filtering=rez)
         else:
@@ -70,6 +78,10 @@ class TypeTitleAPIView(generics.ListAPIView):
 
 
 class HackatonUpdateAPIView(generics.ListAPIView):
+    """
+    апи для новых мероприятий для конкретного приложения(например бота)
+    поьзователь индетифицирутся с помощью токена
+    """
     permission_classes = [IsAuthenticated]
     serializer_class = EventSerializer
 
@@ -79,13 +91,14 @@ class HackatonUpdateAPIView(generics.ListAPIView):
         user = Token.objects.get(key=token).user
 
         try:
-            current_user_history = HistoryUserRequest.objects.get(user_id=user.id)
+            current_user_history = HistoryUserRequest.objects.get(
+                user_id=user.id)
         except ObjectDoesNotExist:
-            current_user_history = HistoryUserRequest.objects.create(user_id=user.id)
+            current_user_history = HistoryUserRequest.objects.create(
+                user_id=user.id)
 
-        print(current_user_history.time_of_last_request)
-
-        result_queryset = Event.objects.filter(date_of_parsing__gte=current_user_history.time_of_last_request)
+        result_queryset = Event.objects.filter(
+            date_of_parsing__gte=current_user_history.time_of_last_request)
         current_user_history.save()
 
         return result_queryset
@@ -95,7 +108,9 @@ class TagAPIView(generics.ListAPIView):
     """
     апи для всех тегов, фильтроции нет
     """
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [
+        IsAuthenticated,
+    ]
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
@@ -104,6 +119,8 @@ class EventTypeAPIView(generics.ListAPIView):
     """
     апи для всех типов, фильтроции нет
     """
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [
+        IsAuthenticated,
+    ]
     queryset = EventTypeClissifier.objects.all()
     serializer_class = EventTypeSerializer
